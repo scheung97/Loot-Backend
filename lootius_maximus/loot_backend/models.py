b56e27c31be1f310f66924edd46691c97703aed8
scheung97 <scheung1997@gmail.com>
2021-02-09 10:04:01 -0500

    product model

--- lootius_maximus/loot_backend/models.py
+++ lootius_maximus/loot_backend/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model): 
+    product_name = models.CharField(max_length = 45)
+    product_price = models.IntegerField(default = 0)
+
+    """
+    not sure if these are needed
+    """
+    product_description = models.CharField(max_length= 10000)
+    product_status = models.BooleanField(max_length=5)
+    def __str__(self): 
+        return self.product_name
