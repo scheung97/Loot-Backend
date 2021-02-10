a25d913becb7701ef3f350f7d702a000ef1c3da9
scheung97 <scheung1997@gmail.com>
2021-02-10 14:26:31 -0500

    categories to model

--- lootius_maximus/loot_backend/models.py
+++ lootius_maximus/loot_backend/models.py
@@ -2,13 +2,25 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model): 
+    ITEM_CATEGORIES = [
+        ('ACC', 'Accessories'), 
+        ('B', 'Bags'), 
+        ('C', 'Clothes'), 
+        ('EL', 'Electronics'), 
+        ('FD', 'Food'), 
+        ('FUR', 'Furniture + Home Deco'), 
+        ('K', 'Kitchen Apps'), 
+        ('NO', 'Not Food'), 
+        ('SH', 'Shoes'), 
+        ('SO', 'Socks'),
+    ]
     product_name = models.CharField(max_length = 45)
     product_price = models.IntegerField(default = 0)
-
+    product_category = models.CharField(max_length=1, choices=ITEM_CATEGORIES)
     """
     not sure if these are needed
     """
     product_description = models.CharField(max_length= 10000)
-    product_status = models.BooleanField(max_length=5)
+    product_status = models.BooleanField(max_length=5, blank = True) #in-queue, liked, in cart, purchased, or disliked
     def __str__(self): 
         return self.product_name
